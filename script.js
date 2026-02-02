054dd3b47bcfebe896441dd27e8d340363187393
Jesus Arambula <elielarambula@gmail.com>
2026-02-02 11:00:22 -0800

    List Views
    List all records and individual record views

--- script.js
+++ script.js
@@ -1,53 +1,127 @@
-penguin_token = "patyHBTGqfGq2LgkL.77f9fb7c5fcbe0ac1ac156d99badda7427da5969548106a83bbc12ecd82086ec";
 
 
-// function for our list view
 async function getAllRecords() {
-  let getResultElement = document.getElementById("brews");
+    let getResultElement = document.getElementById("penguins");
+    const BASE_ID = 'app0pXASqtAQq5o5K';
+    const TABLE_NAME = 'Penguins'; // URL encoded if it has special characters
+    const API_TOKEN = 'patJZHYgn7HJuxd6J.73ee284f043f5f9dfa5fd0316e89526b37a8f296955d9beb3260655935ee49ac';
 
-  const options = {
-    method: "GET",
-    headers: {
-      Authorization: `Bearer patyHBTGqfGq2LgkL.77f9fb7c5fcbe0ac1ac156d99badda7427da5969548106a83bbc12ecd82086ec`,
-    },
-  };
+    const url = `https://api.airtable.com/v0/${BASE_ID}/${TABLE_NAME}`;
 
-  await fetch(
-    `https://api.airtable.com/v0/app0pXASqtAQq5o5K/Penguins`,
-    options
-  )
-    .then((response) => response.json())
-    .then((data) => {
-        console.log(data); // response is an object w/ .records array
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${API_TOKEN}`,
+                'Content-Type': 'application/json'
+            }
+        });
 
-        getResultElement.innerHTML = ""; // clear brews
-
-        let newHtml = "";
-        console.log(data.getAllRecords);
-        for (let i = 0; i < data.records.length; i++) {
-        let logo = data.records[i].fields["Logo"]; // here we are getting column values
-        let name = data.records[i].fields["Name"]; //here we are using the Field ID to fecth the name property
-        let neighborhood = data.records[i].fields["Neighborhood"];
-
-        newHtml += `
-        
-         <div class="col-xl-4 cardImageText">
-          <div class="card list move border-dark mb-5" style="width: 20rem;">
-          <a href="breweries.html?id=${data.records[i].id}">${
-          logo
-            ? `<img class="card-img-top rounded" alt="${name}" src="${logo[0].url}">`
-            : ``
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
         }
-          </a>
-          <p hidden class="card-key">${neighborhood}</p>
-          </div>
-          </div>
-        </div>
-    
-        
-        `;
-      }
+        // Play here ---------------------------------------------------------
+        getResultElement.innerHTML = "";
 
-      getResultElement.innerHTML = newHtml;
-    });
+        let newHTML = ""
+
+        const data = await response.json();
+        console.log(data.records); // An array of record objects
+        console.log(data.records.fields)
+        
+        data.records.forEach(function(record) {
+            let penguinName = record.fields['Name'];
+            let penguinImage = record.fields['Image'][0].url;
+            console.log(record.id)
+            console.log(penguinName);
+            console.log(penguinImage);
+
+            newHTML += `
+            <div class="col-xl-4 cardImageText">
+                <div class="card text-center list move border-dark mb-5 h-100" style="width: 20rem;">
+                    <a href="index.html?id=${record.id}">
+                        <p id="penguin-name">${penguinName}</p>
+                        <img class="card-img-top rounded single" alt="${penguinName} Penguin" src="${penguinImage}">
+                    </a>
+                    <p hidden class="card-key">${penguinName}</p>
+                </div>
+            </div>
+        `
+        });
+
+        getResultElement.innerHTML = newHTML;
+        // Stop playing here ---------------------------------------------------------
+
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+async function getOneRecord(id) {
+    let getResultElement = document.getElementById("penguins");
+    const BASE_ID = 'app0pXASqtAQq5o5K';
+    const TABLE_NAME = 'Penguins'; // URL encoded if it has special characters
+    const API_TOKEN = 'patJZHYgn7HJuxd6J.73ee284f043f5f9dfa5fd0316e89526b37a8f296955d9beb3260655935ee49ac';
+
+    const url = `https://api.airtable.com/v0/${BASE_ID}/${TABLE_NAME}/${id}`;
+
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${API_TOKEN}`,
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        // Play here ---------------------------------------------------------
+        getResultElement.innerHTML = "";
+        let newHTML = ""
+
+        const data = await response.json();
+        console.log(data); // Get single id fields data
+        console.log(data.fields);    
+        
+        let penguinName = data.fields['Name'];
+        let penguinScience = data.fields['Scientific Name'];
+        let penguinDescription = data.fields['Short Description'];
+        let penguinHeight = data.fields['Height'];
+        let penguinWeight = data.fields['Weight'];
+        let penguinImage = data.fields['Image'][0].url;
+
+        newHTML = `
+            <div class="card text-center">
+                <div class="card-header">
+                    ${penguinScience}
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">${penguinName}</h5>
+                    <img class="card-img-top rounded img-fluid" alt="${penguinName} Penguin" src="${penguinImage}" style="height: 50%; width: 50%; object-fit: scale-down;">
+                    <hr>
+                    <p class="card-text">${penguinDescription}</p>
+                </div>
+                <div class="card-footer text-muted">
+                    Height: ${penguinHeight}, Weight: ${penguinWeight}
+                </div>
+            </div>
+        `
+
+        getResultElement.innerHTML = newHTML;
+        // Stop playing here ---------------------------------------------------------
+
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+// getAllRecords();
+
+let idParams = window.location.search.split("?id=");
+if (idParams.length >= 2) {
+  getOneRecord(idParams[1]); // create detail view HTML w/ our id
+} else {
+  getAllRecords(); // no id given, fetch summaries
 }
